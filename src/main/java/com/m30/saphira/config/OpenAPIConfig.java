2397bde84efe0af01b4b13f3065447fbbcca58b7
Pedro <pedrofernandesctt@gmail.com>
2025-09-12 14:04:29 -0300

    initial swagger doc

---
+++ src/main/java/com/m30/saphira/config/OpenAPIConfig.java
@@ -0,0 +1,29 @@
+package com.m30.saphira.config;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI( ) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Software de gerenciamento de portfólio de investimentos com IA.")
+                        .version("v1.0")
+                        .description("API para gerenciamento de investimentos e investidores.")
+                        .contact(new Contact()
+                                .name("Pedro Fernandes")
+                                .email("pedrofernandesctt@gmail.com")
+                                .url(""))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html" ))
+                );
+    }
+}
+
